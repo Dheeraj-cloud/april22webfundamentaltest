a478869c6600e2088db80c856c1247cffc71416b
Dheerajgit-cloud <sharmadheeraj9341@gmail.com>
2022-05-29 21:43:08 +0530

    29/05.2022 test

---
+++ may/Week4/SR.js
@@ -0,0 +1,8 @@
+let l=10;
+let b = 20;
+if(i==b){
+    console.log("Its a Square");
+}
+else{
+    console.log("Its a Rectangle");
+}
---
+++ may/Week4/oddeven.js
@@ -0,0 +1,7 @@
+let num = 8;
+if(num%2==0){
+    console.log("Given number is even");
+}
+else{
+    console.log("Given number is odd");
+}
